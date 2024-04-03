05be31f8c47604d7035297e7e348951d4de4bdc6
Lilian Labat <lilian.labat@epita.fr>
2024-04-03 14:54:40 +0200

    jkhighrh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+mkdir -v -p ~/.local/share/godot/export_templates
+cp -a /root/.local/share/godot/templates/. ~/.local/share/godot/export_templates/
+
 godot "./project.godot" --headless --export-debug "$2" $GITHUB_WORKSPACE/build/$1
